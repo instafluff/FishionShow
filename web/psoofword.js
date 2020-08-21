f8324ab9e666a8e42b227a749276502db4ded8c8
Instafluff <35773245+instafluff@users.noreply.github.com>
2020-08-21 13:27:56 -0700

    Azurequarium!

---
+++ web/psoofword.js
@@ -0,0 +1,2 @@
+window.psoofkey = "AZURE_COMPUTER_VISION_KEY"; https://azure.microsoft.com/en-us/services/cognitive-services/computer-vision/#features
+window.pexelspsoof = "PEXELS_API_KEY"; // Available at https://www.pexels.com/api/?locale=en-US
